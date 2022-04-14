88ea66e3988c873ef1237fba55433935a051e1f6
Sardorbek Makhamadaliev <smakhamadaliev8@gmail.com>
2022-04-14 13:34:07 +0200

    make build

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const form = document.getElementById('form');
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  form.reset();
-  window.scrollTo(0, 0);
-});
